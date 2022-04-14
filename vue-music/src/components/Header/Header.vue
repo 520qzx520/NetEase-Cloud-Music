332a1020d178f627c55800c63294197531fa02e8
qzx <2351788920@qq.com>
2022-04-14 13:35:51 +0800

    完成登录页面

--- vue-music/src/components/Header/Header.vue
+++ vue-music/src/components/Header/Header.vue
@@ -25,7 +25,7 @@
       </div>
       <div class="right-list">
         <ul>
-           <!-- <li> <input type="text" style="width: 158px" placeholder="音乐/专辑/歌手" /></li>  -->
+          <!-- <li> <input type="text" style="width: 158px" placeholder="音乐/专辑/歌手" /></li>  -->
           <router-link to="/search" custom v-slot="{ navigate }">
             <div @click="navigate" @keypress.enter="navigate" role="link">
               <li>
@@ -33,12 +33,128 @@
                   type="text"
                   style="width: 158px"
                   placeholder="音乐/专辑/歌手"
+                  class="input"
                 />
               </li>
             </div>
           </router-link>
           <li>创作者中心</li>
-          <li><a>登录</a></li>
+          <li class="login">
+            <a @click="LoginBtn">登录</a>
+            <div class="login-box" v-show="LoginTF">
+              <div class="title">
+                <a>登录</a>
+                <a class="close" @click="Close">X</a>
+              </div>
+              <div class="login-contain">
+                <!-- 扫码登录 -->
+                <div v-show="LoginIndex" class="LoginIndex">
+                  <div class="flex">
+                    <div class="img"></div>
+                    <div class="right">
+                      <h3 style="font-size: 16px">扫码登录</h3>
+                      <img src="./img/user.jpg" />
+                      <a>使用 网易云音乐APP扫码登录</a>
+                    </div>
+                  </div>
+                  <div class="morelogin" @click="moreLoginBtn">
+                    <a>其他登录方式</a>
+                  </div>
+                </div>
+                <!-- 各种登录页面 -->
+                <div class="moreLoginBox" v-show="everyLogin">
+                  <div class="moreLoginBox-flex">
+                    <div class="more-left">
+                      <img src="./img/morelogin.png" />
+                      <div class="phoneBox">
+                        <a class="phonelogin" @click="phoneLoginBtn">
+                          手机登录
+                        </a>
+                        <a class="phoneresgin">手机注册</a>
+                      </div>
+                    </div>
+                    <div class="more-right">
+                      <div class="icons">
+                        <ul>
+                          <li class="wx">
+                            <i></i>
+                            <a>微信登录</a>
+                          </li>
+                          <li class="qq">
+                            <i></i>
+                            <a>QQ登录</a>
+                          </li>
+                          <li class="wb">
+                            <i></i>
+                            <a>微博登录</a>
+                          </li>
+                          <li class="wy">
+                            <i></i>
+                            <a>网易邮箱账号登录</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="bottom">
+                    <div class="agree">
+                      <input type="checkbox" id="inputbox" />
+                      <label for="inputbox">同意</label>
+                      <a>《服务条款》《隐私策略》《儿童隐私政策》</a>
+                    </div>
+                    <div class="code" @click="codeBtn">
+                      <img src="./img/code.png" />
+                    </div>
+                  </div>
+                </div>
+
+                <!-- 手机号登录 -->
+                <div class="phologin" v-show="phoneLoginFt">
+                  <div class="phmargin">
+                    <div class="phcontain">
+                      <input
+                        type="text"
+                        placeholder="请输入手机号"
+                        class="ph"
+                      />
+                      <div v-show="passInput">
+                        <input
+                          type="text"
+                          placeholder="请输入验证码"
+                          class="cod"
+                        />
+                        <span>获取验证码</span>
+                        <div class="passlogin">
+                          <a @click="passInputBtn">密码登录</a>
+                          <a>自动登录</a>
+                        </div>
+                      </div>
+
+                      <div v-show="!passInput">
+                        <input
+                          type="text"
+                          placeholder="请输入密码"
+                          class="cod"
+                          style="width: 100%"
+                        />
+
+                        <div class="passlogin">
+                          <a @click="passInputBtn">验证码登录</a>
+                          <a>自动登录</a>
+                        </div>
+                      </div>
+
+                      <a class="loginbtn">登录</a>
+                    </div>
+                  </div>
+                  <div class="alla">
+                    <a @click="phoneMoreLoginBtn">其他方式登录</a>
+                    <a>没有账号去注册</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
@@ -46,7 +162,53 @@
 </template>
 
 <script>
-  export default {};
+  export default {
+    data() {
+      return {
+        LoginIndex: true,
+        everyLogin: false,
+        LoginTF: false,
+        phoneLoginFt: false,
+        passInput: true,
+      };
+    },
+    methods: {
+      //导航栏登录按钮
+      LoginBtn() {
+        this.LoginTF = true;
+      },
+      //关闭按钮
+      Close() {
+        this.LoginTF = false;
+      },
+      //登录首页的更多登录方式
+      moreLoginBtn() {
+        this.LoginIndex = !this.LoginIndex;
+        this.everyLogin = !this.everyLogin;
+      },
+      //手机登录
+      phoneLoginBtn() {
+        this.everyLogin = !this.everyLogin;
+        this.phoneLoginFt = !this.phoneLoginFt;
+      },
+
+      //其他登录方式
+      phoneMoreLoginBtn() {
+        this.phoneLoginFt = !this.phoneLoginFt;
+        this.everyLogin = !this.everyLogin;
+      },
+
+      //右下角二维码点击
+      codeBtn() {
+        this.everyLogin = !this.everyLogin;
+        this.LoginIndex = !this.LoginIndex;
+      },
+      //密码登陆
+      passInputBtn() {
+        this.passInput = !this.passInput;
+      },
+    },
+  };
 </script>
 
 <style scoped lang="less">
@@ -115,7 +277,7 @@
           li {
             font-size: 12px;
             margin: 0 20px;
-            input {
+            .input {
               box-sizing: border-box;
               height: 30px;
               padding-left: 30px;
@@ -123,6 +285,243 @@
               border-radius: 32px;
               font-size: 12px;
               background-color: white;
+            }
+          }
+          .login {
+            .login-box {
+              position: absolute;
+              height: 400px;
+              width: 550px;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              background-color: white;
+              z-index: 999999999;
+              color: black;
+              border: 1px solid #ddd;
+              .title {
+                height: 40px;
+                line-height: 40px;
+                background-color: #ddd;
+                a {
+                  color: black;
+                  font-size: 14px;
+                  margin-left: 20px;
+                }
+                .close {
+                  float: right;
+                  margin-right: 20px;
+                }
+              }
+
+              .login-contain {
+                margin-top: 30px;
+                height: 250px;
+                .LoginIndex {
+                  .flex {
+                    display: flex;
+                    justify-content: center;
+                    .img {
+                      width: 125px;
+                      height: 220px;
+                      margin-left: 60px;
+                      background: url(./img/login_qr.fe695c03.png);
+                      background-size: 100%;
+                    }
+
+                    .right {
+                      display: flex;
+                      flex-direction: column;
+                      width: 300px;
+                      justify-content: center;
+                      align-items: center;
+
+                      img {
+                        width: 150px;
+                        height: 150px;
+                        margin: 20px 0;
+                      }
+                      a {
+                        color: black;
+                      }
+                    }
+                  }
+
+                  .morelogin {
+                    margin-top: 45px;
+                    text-align: center;
+                    a {
+                      color: black;
+                      border-radius: 5px;
+                      background: #ddd;
+                      padding: 7px;
+                    }
+                  }
+                }
+
+                .moreLoginBox {
+                  margin-top: 35px;
+                  .moreLoginBox-flex {
+                    display: flex;
+                    .more-left {
+                      margin-left: 50px;
+                      width: 300px;
+                      height: 270px;
+                      img {
+                        width: 225px;
+                        height: 120px;
+                      }
+
+                      .phoneBox {
+                        a {
+                          display: inline-block;
+                          width: 250px;
+                          height: 30px;
+                          line-height: 30px;
+                          text-align: center;
+                        }
+                        .phonelogin {
+                          background: cyan;
+                          margin: 20px 0;
+                        }
+                        .phoneresgin {
+                          background: rgb(117, 117, 117);
+                        }
+                      }
+                    }
+
+                    .more-right {
+                      width: 250px;
+                      height: 240px;
+
+                      .icons {
+                        ul {
+                          height: 240px;
+                          display: flex;
+                          flex-direction: column;
+                          justify-content: space-evenly;
+                          align-items: flex-start;
+                          li {
+                            display: flex;
+                            align-items: center;
+
+                            i {
+                              display: inline-block;
+                              width: 38px;
+                              height: 38px;
+                            }
+
+                            a {
+                              margin-left: 20px;
+                              color: black;
+                            }
+                          }
+
+                          .wx {
+                            i {
+                              background: url(./img/icons.png) no-repeat -150px -670px;
+                            }
+                          }
+
+                          .qq {
+                            i {
+                              background: url(./img/icons.png) no-repeat -190.5px -670px;
+                            }
+                          }
+                          .wb {
+                            i {
+                              background: url(./img/icons.png) no-repeat -231px -670px;
+                            }
+                          }
+                          .wy {
+                            i {
+                              background: url(./img/icons.png) no-repeat -271px -670px;
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+
+                  .bottom {
+                    position: relative;
+                    .agree {
+                      margin-left: 50px;
+                      display: flex;
+                      align-items: center;
+                      #inputbox {
+                        margin-right: 5px;
+                        border: 1px solid rgb(119, 28, 28);
+                        width: 15px;
+                        height: 15px;
+                      }
+
+                      a {
+                        color: black;
+                      }
+                    }
+                    .code {
+                      position: absolute;
+                      right: 0;
+                      top: 3px;
+                      img {
+                        width: 52px;
+                        height: 52px;
+                      }
+                    }
+                  }
+                }
+
+                .phologin {
+                  a {
+                    color: black;
+                  }
+                  .phmargin {
+                    height: 250px;
+                    width: 300px;
+                    margin: 30px auto;
+
+                    .phcontain {
+                      input {
+                        margin-top: 20px;
+                        box-sizing: border-box;
+                        width: 100%;
+                        color: black;
+                        height: 40px;
+                        border: 1px solid #dddd;
+                        padding-left: 10px;
+                      }
+                      .cod {
+                        width: 66%;
+                        margin-right: 20px;
+                      }
+                      span {
+                        border: 1px solid #ddd;
+                        padding: 11px 10px;
+                      }
+
+                      .passlogin {
+                        display: flex;
+                        margin-top: 20px;
+                        justify-content: space-between;
+                      }
+                      .loginbtn {
+                        width: 100%;
+                        display: inline-block;
+                        margin-top: 15px;
+                        background: rgb(87, 87, 241);
+                        padding: 11px 0;
+                        text-align: center;
+                      }
+                    }
+                  }
+
+                  .alla {
+                    display: flex;
+                    justify-content: space-around;
+                  }
+                }
+              }
             }
           }
         }
